b99ce4b13fb5e65deac6fc297b135556a72e9dee
Djibh <julien.riera@protonmail.com>
2023-08-05 22:15:33 +0200

    feat(F02:LOGIN) > add redirection from ErrorPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+      <h1>Error page</h1>
+      <Link to="/"><button>Retourner vers la page d&apos;accueil</button></Link>
+    </div>
   )
 }
