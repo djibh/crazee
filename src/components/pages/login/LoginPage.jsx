e9e6435fd3a92b7102d2757246631a6c37ce25e0
Djibh <julien.riera@protonmail.com>
2023-08-08 13:49:40 +0200

    style(F03:LOGIN) > Add logo and font

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -16,14 +16,17 @@ const LoginPageStyled = styled.div`
   justify-content: center;
   align-items: center;
 
-  ::before {
+  &::before {
     content: "";
     background: url("/src/assets/img/burger-background.jpg") rgba(0, 0, 0, 0.7);
     background-size: cover;
     background-position: center;
     background-blend-mode: darken;
     position: absolute;
-    inset: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
     z-index: -1;
   }
 `
