5e7aaa5e94aad937c7764c7a5effe38d5f003e77
Djibh <julien.riera@protonmail.com>
2023-09-01 09:33:31 +0200

    WIP (F04:ORDER) > Create <Navbar /> layout

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -5,14 +5,30 @@ import { styled } from "styled-components";
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-        <h1>Bonjour { username }</h1>
-        <Link to="/"><button>Déconnexion</button></Link>
+      <div className="left-side">LEFT SIDE</div>
+        <div className="right-side">
+          <h1>Bonjour { username }</h1>
+          <Link to="/"><button>Déconnexion</button></Link>
+        </div>
     </NavbarStyled>
   )
 }
 const NavbarStyled = styled.nav`
     background: blue;
     height: 10vh;
+    display: flex;
+    justify-content: space-between;
+    
+    h1 {
+      font-size: 1rem;
+    }
+    .left-side {
+      background: pink;
+    }
+    .right-side {
+      background: purple;
+    }
+
 `;
 
 Navbar.propTypes = {
