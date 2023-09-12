fe2df57663e87b90f5595ad94e1b22e5e0827b70
Djibh <julien.riera@protonmail.com>
2023-09-12 14:04:37 +0200

    Refactor (ORDER) > MENU > Remove useless reformatting from maths.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import LoginPage from './components/pages/login/LoginPage'
 import OrderPage from './components/pages/order/OrderPage'
 import ErrorPage from './components/pages/error/ErrorPage'
+import './App.css'
 
 function App() {
   return <Routes>
--- src/utils/maths.ts
+++ src/utils/maths.ts
@@ -1,18 +1,12 @@
-export function formatPrice(priceToFormat) {
+export function formatPrice(priceToFormat: number) {
   let price = priceToFormat
 
   // @TODO: perhaps change this to if(!price) return 0
   if (!price) return "0,00 €"
-  price = replaceFrenchCommaWithDot(price)
 
   const formattedPrice = new Intl.NumberFormat("fr-FR", {
     style: "currency",
     currency: "EUR",
   }).format(price)
   return formattedPrice
-}
-
-export function replaceFrenchCommaWithDot(price) {
-  if (typeof price === "string") price = parseFloat(price.replace(",", "."))
-  return price
 }
