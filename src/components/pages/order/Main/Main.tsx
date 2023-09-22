df79cc5488b9cc5118d7b03f920d3a16c76d0bd4
Djibh <julien.riera@protonmail.com>
2023-09-22 11:36:05 +0200

    feat (F07:ORDER) > Main > Add layout for admin panel

--- src/components/pages/order/Main/Main.tsx
+++ src/components/pages/order/Main/Main.tsx
@@ -1,25 +1,40 @@
 import { styled } from "styled-components";
-// import Basket from "./Basket";
 import Menu from "./Menu";
 import { theme } from "../../../../theme";
+// import Basket from "./Basket";
 
 export default function Main() {
   return (
     <MainStyled>
       {/* <Basket /> */}
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">ADMIN</div>
+      </div>
     </MainStyled>
   )
 }
 
 const MainStyled = styled.main`
     display: grid;
-    /* grid-template-columns: 25% 1fr; For <Basket /> later on*/ 
+    /* grid-template-columns: 25% 1fr;  will be used when basket implemented */
     grid-template-columns: 1fr;
     background: ${theme.colors.background_white};
     flex: 1;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
-    overflow-y: scroll;
+    overflow-y: hidden;
+
+    .menu-and-admin {
+      overflow-y: hidden;
+      display: grid;
+
+      .admin {
+        background: rgba(255, 192, 203, 0.85);
+        height: 250px;
+        border-bottom-right-radius: ${theme.borderRadius.extraRound};
+        border-bottom-left-radius: ${theme.borderRadius.extraRound};
+      }
+    }
 `;
--- src/components/pages/order/Main/Menu.tsx
+++ src/components/pages/order/Main/Menu.tsx
@@ -25,10 +25,11 @@ const [menu, setmenu] = useState(fakeMenu2)
 
 const MenuStyled = styled.section`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
   background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  overflow-y: scroll;
 `;
